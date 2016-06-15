cab31b723f2a9a35fe6109f4af1f7607882931d5
Hervé Ménager <hmenager@pasteur.fr>
2016-06-15 23:08:40 +0200

    correct file type, add namespaces and schemas, minor corrections.

--- pyacd/acd_2_cwl.py
+++ pyacd/acd_2_cwl.py
@@ -39,40 +39,55 @@ DATATYPES_CWL = {'array': {'type': 'array', 'item': 'int'},
                  'boolean': {'type': 'boolean'},
                  'integer': {'type': 'int'},
                  'float': {'type': 'float'},
-                 'range': {'type': 'file'},
-                 'regexp': {'type': 'file'},
-                 'pattern': {'type': 'file'},
+                 'range': {'type': 'File'},
+                 'regexp': {'type': 'File'},
+                 'pattern': {'type': 'File'},
                  'string': {'type': 'string'},
-                 'toggle': {'type': 'file'},
-                 'codon': {'type': 'file'},
-                 'cpdb': {'type': 'file'},
-                 'datafile': {'type': 'file'},
-                 'directory': {'type': 'file'},
-                 'dirlist': {'type': 'file'},
-                 'discretestates': {'type': 'file'},
-                 'distances': {'type': 'file'},
-                 'features': {'type': 'file'},
-                 'filelist': {'type': 'file'},
-                 'frequencies': {'type': 'file'},
-                 'infile': {'type': 'file'},
-                 'matrix': {'type': 'file'}, 'matrixf': {'type': 'file'},
-                 'properties': {'type': 'file'},
-                 'scop': {'type': 'file'}, 'sequence': {'type': 'file'}, 'seqall': {'type': 'file'},
-                 'seqset': {'type': 'file'}, 'seqsetall': {'type': 'file'},
-                 'tree': {'type': 'file'},
-                 'list': {'type': 'file'}, 'selection': {'type': 'file'},
-                 'align': {'type': 'file'}, 'featout': {'type': 'file'}, 'outcodon': {'type': 'file'},
-                 'outdata': {'type': 'file'}, 'outdir': {'type': 'file'},
-                 'outdiscrete': {'type': 'file'},
-                 'outdistance': {'type': 'file'},
-                 'outfile': {'type': 'file'}, 'outfileall': {'type': 'file'}, 'outfreq': {'type': 'file'},
-                 'outmatrix': {'type': 'file'},
-                 'outmatrixf': {'type': 'file'}, 'outproperties': {'type': 'file'},
-                 'outscop': {'type': 'file'}, 'outtree': {'type': 'file'}, 'report': {'type': 'file'},
-                 'seqout': {'type': 'file'}, 'seqoutall': {'type': 'file'},
-                 'seqoutset': {'type': 'file'},
-                 'graph': {'type': 'file'}, 'xygraph': {'type': 'file'}}
+                 'toggle': {'type': 'File'},
+                 'codon': {'type': 'File'},
+                 'cpdb': {'type': 'File'},
+                 'datafile': {'type': 'File'},
+                 'directory': {'type': 'File'},
+                 'dirlist': {'type': 'File'},
+                 'discretestates': {'type': 'File'},
+                 'distances': {'type': 'File'},
+                 'features': {'type': 'File'},
+                 'filelist': {'type': 'File'},
+                 'frequencies': {'type': 'File'},
+                 'infile': {'type': 'File'},
+                 'matrix': {'type': 'File'}, 'matrixf': {'type': 'File'},
+                 'properties': {'type': 'File'},
+                 'scop': {'type': 'File'}, 'sequence': {'type': 'File'}, 'seqall': {'type': 'File'},
+                 'seqset': {'type': 'File'}, 'seqsetall': {'type': 'File'},
+                 'tree': {'type': 'File'},
+                 'list': {'type': 'File'}, 'selection': {'type': 'File'},
+                 'align': {'type': 'File'}, 'featout': {'type': 'File'}, 'outcodon': {'type': 'File'},
+                 'outdata': {'type': 'File'}, 'outdir': {'type': 'File'},
+                 'outdiscrete': {'type': 'File'},
+                 'outdistance': {'type': 'File'},
+                 'outfile': {'type': 'File'}, 'outfileall': {'type': 'File'}, 'outfreq': {'type': 'File'},
+                 'outmatrix': {'type': 'File'},
+                 'outmatrixf': {'type': 'File'}, 'outproperties': {'type': 'File'},
+                 'outscop': {'type': 'File'}, 'outtree': {'type': 'File'}, 'report': {'type': 'File'},
+                 'seqout': {'type': 'File'}, 'seqoutall': {'type': 'File'},
+                 'seqoutset': {'type': 'File'},
+                 'graph': {'type': 'File'}, 'xygraph': {'type': 'File'}}
 
+NAMESPACES_AND_SCHEMAS = {'$namespaces': {
+  'dct': 'http://purl.org/dc/terms/',
+  'foaf': 'http://xmlns.com/foaf/0.1/',
+  'doap': 'http://usefulinc.com/ns/doap#',
+  'adms': 'http://www.w3.org/ns/adms#',
+  'dcat': 'http://www.w3.org/ns/dcat#',
+  'edam': 'http://edamontology.org/'},
+  '$schemas': [
+    'http://dublincore.org/2012/06/14/dcterms.rdf',
+    'http://xmlns.com/foaf/spec/20140114.rdf',
+    'http://usefulinc.com/ns/doap#',
+    'http://www.w3.org/ns/adms#',
+    'http://www.w3.org/ns/dcat.rdf',
+    'http://edamontology.org/EDAM.owl']
+}
 
 def get_cwl(acdDef):
     inputs = []
@@ -81,20 +96,25 @@ def get_cwl(acdDef):
         for parameter in section.parameters:
             cwl_parameter = copy.deepcopy(DATATYPES_CWL[parameter.datatype])
             cwl_parameter['id'] = parameter.name
+            cwl_parameter['label'] = parameter.attributes['information'] if parameter.attributes['information']!='' else parameter.attributes['prompt']
+            cwl_parameter['description'] = parameter.attributes['help'] or cwl_parameter['label']
             if DATATYPES[parameter.datatype] == 'input':
                 cwl_parameter['inputBinding'] = {'prefix': '--' + parameter.name,
                                                  'position': len(inputs) + 1}
+                cwl_parameter['default'] = parameter.attributes['default']
                 inputs.append(cwl_parameter)
             else:
                 cwl_parameter['outputBinding'] = {}
                 outputs.append(cwl_parameter)
-    return {'cwlVersion': 'cwl:draft3',
+    acd_cwl = {'cwlVersion': 'cwl:draft-3',
             'class': 'CommandLineTool',
             'baseCommand': acdDef.application.name,
             'description': acdDef.application.attributes['documentation'],
             'inputs': inputs,
             'outputs': outputs
             }
+    acd_cwl.update(NAMESPACES_AND_SCHEMAS)
+    return acd_cwl
 
 
 def print_datatype_parameter_class_mapping():

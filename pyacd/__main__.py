b8ba39d683a81b6e26ff85faeedae5983da9c73a
Hervé Ménager <hmenager@pasteur.fr>
2016-06-15 22:35:16 +0200

    dump CWL to files in a directory, correct parameter prompts

--- pyacd/__main__.py
+++ pyacd/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import argparse
 import glob
 
@@ -11,14 +12,16 @@ from .acd_2_cwl import get_cwl, print_datatype_parameter_class_mapping
 
 def main():
     arg_parser = argparse.ArgumentParser(description='EMBOSS ACD file parser')
-    arg_parser.add_argument('--files', help="Mobyle XML files to import definitions from")
+    arg_parser.add_argument('--files', help="ACD files to process")
+    arg_parser.add_argument('--destination', help="directory to store the CWL files to")
     args = arg_parser.parse_args()
     parser = AcdParser()
     for file in glob.glob(args.files):
         try:
             print "processing file {0}...".format(file)
             acd_object = parser.parse_acd(open(file,'r').read())
-            print dump(get_cwl(acd_object), default_flow_style=False)
+            print dump(get_cwl(acd_object), open(os.path.join(args.destination, os.path.basename(file)+'.cwl'),'w'),
+                       default_flow_style=False)
         except ParseException as pexc:
             print "Error while parsing file {0}: {1}".format(file, pexc)
         except UnknownAcdPropertyException as upexc:

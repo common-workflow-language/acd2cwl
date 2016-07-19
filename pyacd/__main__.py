2a565a34bab4e375765a8b62f6e16099a1dd6fa2
Hervé Ménager <hmenager@pasteur.fr>
2016-07-19 10:12:47 +0200

    make pylint happy

--- pyacd/__main__.py
+++ pyacd/__main__.py
@@ -1,3 +1,6 @@
+"""
+  main module to parse ACD files and generate CWL files
+"""
 import sys
 import os
 import argparse
@@ -7,24 +10,28 @@ from pyparsing import ParseException
 
 from .parser import parse_acd
 from .acd import UnknownAcdPropertyException
-from .acd_2_cwl import get_cwl, print_datatype_parameter_class_mapping
+from .acd_2_cwl import get_cwl
 
 def main():
+    """
+    main function
+    """
     arg_parser = argparse.ArgumentParser(description='EMBOSS ACD file parser')
     arg_parser.add_argument('files', help="ACD files to process", nargs='+')
-    arg_parser.add_argument('--destination', help="directory to store the CWL files to", default='/tmp')
+    arg_parser.add_argument('--destination', help="directory to store the CWL files to",
+                            default='/tmp')
     args = arg_parser.parse_args()
-    for file in args.files:
+    for acd_file in args.files:
         try:
-            print "processing file {0}...".format(file)
-            acd_object = parse_acd(open(file,'r').read())
-            dump(get_cwl(acd_object), open(os.path.join(args.destination, os.path.basename(file)+'.cwl'),'w'),
-                       default_flow_style=False)
+            print "processing file {0}...".format(acd_file)
+            acd_object = parse_acd(open(acd_file, 'r').read())
+            dump(get_cwl(acd_object),
+                 open(os.path.join(args.destination, os.path.basename(acd_file)+'.cwl'), 'w'),
+                 default_flow_style=False)
         except ParseException as pexc:
-            print "Error while parsing file {0}: {1}".format(file, pexc)
+            print "Error while parsing file {0}: {1}".format(acd_file, pexc)
         except UnknownAcdPropertyException as upexc:
-            print "Error while parsing file {0}: {1}".format(file, upexc.message)
+            print "Error while parsing file {0}: {1}".format(acd_file, upexc.message)
 
 if __name__ == "__main__":
-    #print_datatype_parameter_class_mapping()
     sys.exit(main())

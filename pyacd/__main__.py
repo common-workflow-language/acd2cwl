a5caf5a288a0fd518d8da5d420497672b562f563
Hervé Ménager <hmenager@pasteur.fr>
2016-07-22 15:28:44 +0200

    formatting

--- pyacd/__main__.py
+++ pyacd/__main__.py
@@ -12,13 +12,15 @@ from .parser import parse_acd
 from .acd import UnknownAcdPropertyException
 from .acd_2_cwl import get_cwl
 
+
 def main():
     """
     main function
     """
     arg_parser = argparse.ArgumentParser(description='EMBOSS ACD file parser')
     arg_parser.add_argument('files', help="ACD files to process", nargs='+')
-    arg_parser.add_argument('--destination', help="directory to store the CWL files to",
+    arg_parser.add_argument('--destination',
+                            help="directory to store the CWL files to",
                             default='/tmp')
     args = arg_parser.parse_args()
     for acd_file in args.files:
@@ -26,12 +28,14 @@ def main():
             print "processing file {0}...".format(acd_file)
             acd_object = parse_acd(open(acd_file, 'r').read())
             dump(get_cwl(acd_object),
-                 open(os.path.join(args.destination, os.path.basename(acd_file)+'.cwl'), 'w'),
+                 open(os.path.join(args.destination,
+                                   os.path.basename(acd_file)+'.cwl'), 'w'),
                  default_flow_style=False)
         except ParseException as pexc:
             print "Error while parsing file {0}: {1}".format(acd_file, pexc)
         except UnknownAcdPropertyException as upexc:
-            print "Error while parsing file {0}: {1}".format(acd_file, upexc.message)
+            print "Error while parsing file {0}: {1}".format(acd_file,
+                                                             upexc.message)
 
 if __name__ == "__main__":
     sys.exit(main())

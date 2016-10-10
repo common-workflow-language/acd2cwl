4822eee1280745e6d7113f33a9d81aa69e2899db
Hervé Ménager <hmenager@pasteur.fr>
2016-10-10 16:51:44 +0200

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ readme = open(README).read()
 
 setup(
     name='acd2cwl',
-    version='0.1.3',
+    version='0.1.4',
     description='CWL generator for ACD files',
     long_description=readme,
     keywords=['EMBOSS','ACD','Common Workflow Language', 'CWL'],
